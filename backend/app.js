0981936eacaa083f2f220a96bb5a25ba5e27af0e
Vladimir Chehnousov <tehnokiparis@mail.ru>
2023-07-08 16:35:26 +0300

    ПР15 test2

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,7 @@ const NotFoundError = require('./errors/not-found-error');
 const { PORT = 4000 } = process.env;
 
 mongoose
-  .connect('mongodb://127.0.0.1:27017/mestodb', {
+  .connect('mongodb://localhost:3000/mestodb', {
     useNewUrlParser: true,
   })
   .then(() => {
